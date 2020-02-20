62d8b05afe49180148c51dde5592caa670b9b9df
tniita <tniita@TNIITA-JP.jp.oracle.com>
2020-02-20 17:04:35 +0900

    v0.12版作成

--- src/vcn.tf
+++ src/vcn.tf
@@ -15,8 +15,8 @@
 ### VCN ###
 
 resource "oci_core_virtual_network" "vcn_client" {
-  cidr_block     = "${var.vcn_cidr_block}"
-  compartment_id = "${var.compartment_ocid}"
+  cidr_block     = var.vcn_cidr_block
+  compartment_id = var.compartment_ocid
   display_name   = "vcn_client"
   dns_label      = "vcn"
 
@@ -28,21 +28,21 @@ resource "oci_core_virtual_network" "vcn_client" {
 ### Internet Gateay ###
 
 resource "oci_core_internet_gateway" "igw" {
-  compartment_id = "${var.compartment_ocid}"
+  compartment_id = var.compartment_ocid
   display_name   = "igw_client"
-  vcn_id         = "${oci_core_virtual_network.vcn_client.id}"
+  vcn_id         = oci_core_virtual_network.vcn_client.id
 }
 
 ### Route Table ###
 
 resource "oci_core_route_table" "rt1" {
-  compartment_id = "${var.compartment_ocid}"
-  vcn_id         = "${oci_core_virtual_network.vcn_client.id}"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.vcn_client.id
   display_name   = "rt1_client"
 
   route_rules {
     destination       = "0.0.0.0/0"
-    network_entity_id = "${oci_core_internet_gateway.igw.id}"
+    network_entity_id = oci_core_internet_gateway.igw.id
   }
 }
 
@@ -50,82 +50,82 @@ resource "oci_core_route_table" "rt1" {
 
 resource "oci_core_security_list" "sl" {
   display_name   = "sl-loadbalancer"
-  compartment_id = "${var.compartment_ocid}"
-  vcn_id         = "${oci_core_virtual_network.vcn_client.id}"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.vcn_client.id
 
-  egress_security_rules = [{
+  egress_security_rules {
     protocol    = "all"
     destination = "0.0.0.0/0"
-  }]
+  }
 
-  ingress_security_rules = [{
+  ingress_security_rules {
     tcp_options {
-      "max" = 22
-      "min" = 22
+      max = 22
+      min = 22
     }
 
     protocol = "6"
     source   = "0.0.0.0/0"
-  },
-    {
-      tcp_options {
-        "max" = 80
-        "min" = 80
-      }
+  }
+  ingress_security_rules {
+    tcp_options {
+      max = 80
+      min = 80
+    }
 
-      protocol = "6"
-      source   = "0.0.0.0/0"
-    },
-    {
-      tcp_options {
-        "max" = 443
-        "min" = 443
-      }
+    protocol = "6"
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    tcp_options {
+      max = 443
+      min = 443
+    }
 
-      protocol = "6"
-      source   = "0.0.0.0/0"
-    },
-    {
-      icmp_options {
-        "type" = 0
-      }
+    protocol = "6"
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = 0
+    }
 
-      protocol = 1
-      source   = "0.0.0.0/0"
-    },
-    {
-      icmp_options {
-        "type" = 3
-        "code" = 4
-      }
+    protocol = 1
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = 3
+      code = 4
+    }
 
-      protocol = 1
-      source   = "0.0.0.0/0"
-    },
-    {
-      icmp_options {
-        "type" = 8
-      }
+    protocol = 1
+    source   = "0.0.0.0/0"
+  }
+  ingress_security_rules {
+    icmp_options {
+      type = 8
+    }
 
-      protocol = 1
-      source   = "0.0.0.0/0"
-    },
-  ]
+    protocol = 1
+    source   = "0.0.0.0/0"
+  }
 }
 
 ### Subnet  ###
 
 resource "oci_core_subnet" "subnet1" {
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ashburn.availability_domains[0],"name")}"
-  cidr_block          = "${var.subnet_cidr_w1}"
+  availability_domain = data.oci_identity_availability_domains.ashburn.availability_domains[0]["name"]
+  cidr_block          = var.subnet_cidr_w1
   display_name        = "subnet1-AD1"
-  security_list_ids   = ["${oci_core_security_list.sl.id}"]
-  compartment_id      = "${var.compartment_ocid}"
-  vcn_id              = "${oci_core_virtual_network.vcn_client.id}"
-  route_table_id      = "${oci_core_route_table.rt1.id}"
-  dhcp_options_id     = "${oci_core_virtual_network.vcn_client.default_dhcp_options_id}"
+  security_list_ids   = [oci_core_security_list.sl.id]
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_virtual_network.vcn_client.id
+  route_table_id      = oci_core_route_table.rt1.id
+  dhcp_options_id     = oci_core_virtual_network.vcn_client.default_dhcp_options_id
 
   provisioner "local-exec" {
     command = "sleep 5"
   }
 }
+
